9c157ab53aa93230ee6e53e5f8f1a4c8426b5397
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-11-30 13:01:31 +0100

    Add Iteration #2

--- functions.js
+++ functions.js
@@ -3,18 +3,25 @@
 // Iteración #1 - Busca el máximo
 // Completa la función que tomando dos números como argumento devuelva el más alto.
 
-function sum(numberOne , numberTwo) {
+// function sum(numberOne , numberTwo) {
     
-    if(numberOne > numberTwo){
-        console.log("El número " + numberOne + " es el mayor.");
+//     if(numberOne > numberTwo){
+//         console.log("El número " + numberOne + " es el mayor.");
 
-    } else if(numberTwo > numberOne){
-        console.log("El número " + numberTwo + " es el mayor.");
-    }
-}
-sum(18, 56);
-
+//     } else if(numberTwo > numberOne){
+//         console.log("El número " + numberTwo + " es el mayor.");
+//     }
+// }
+// sum(18, 56);
 
 
 // Iteración #2 - Buscar la palabra más larga
 // Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero. Puedes usar este array para probar tu función:
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+    // insert code
+}
+
+console.log(avengers.length);
