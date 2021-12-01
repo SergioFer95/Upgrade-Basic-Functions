857c20c7efdad452173f70c9bcca6728f4864499
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-02 00:04:40 +0100

    Finish Iteration #5

--- functions.js
+++ functions.js
@@ -3,16 +3,20 @@
 // Iteración #1 - Busca el máximo
 // Completa la función que tomando dos números como argumento devuelva el más alto.
 
-// function sum(numberOne , numberTwo) {
-    
-//     if(numberOne > numberTwo){
-//         console.log("El número " + numberOne + " es el mayor.");
+var num1 = parseInt(prompt("Introduce un número", 0));
+var num2 = parseInt(prompt("Introduce otro número", 0));
+alert("Te vamos a decir cuál de los dos números es el mayor");
 
-//     } else if(numberTwo > numberOne){
-//         console.log("El número " + numberTwo + " es el mayor.");
-//     }
-// }
-// sum(18, 56);
+function sum(numberOne , numberTwo) {
+    
+    if(numberOne > numberTwo){
+        console.log("El número " + numberOne + " es el mayor.");
+
+    } else if(numberTwo > numberOne){
+        console.log("Este es el resultado de la Iteración #1: " + "El número " + numberTwo + " es el mayor.");
+    }
+}
+sum(num1, num2);
 
 
 // Iteración #2 - Buscar la palabra más larga
@@ -32,36 +36,67 @@ function findLongestWord(param) {
 }
 
 let solution = findLongestWord(avengers);
-console.log(solution);
+console.log("Este es el resultado de la Iteración #2: " + solution);
 
 // Iteración #3: Calcular la suma
 // Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
 // Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.
 
+// OPCIÓN 1: resuelta!!!!!!!!
 const allNumbers = [1, 2, 3, 5, 45, 37, 58];
 
 var suma = (num1, num2) => num1 + num2;
 
-console.log(allNumbers.reduce(suma));
-
+console.log("Este es el resultado de la Iteración #3: " + allNumbers.reduce(suma));
 
 
 
 // Iteración #4: Calcular el promedio
 // Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
-const AllNumbers2 = [12, 21, 38, 5, 45, 37, 6];
+const allNumbers2 = [12, 21, 38, 5, 45, 37, 6];
+
 function average(param) {
-  // insert code
+    var allNumbs = 0;
+    for(var i = 0; i < param.length; i++){
+    }
 }
 
+var resultado = average(allNumbers2);
+console.log("Este es el resultado de la Iteración #4: " + resultado);
+
+// function average(param) {
+//      var suma = param[0];
+
+//     for(var i = 0; i < suma; i++){
+//         if(i < param[0] || i > param[0]){
+//             var total = suma + param[i];
+//         }
+//     }
+//     return (total);
+// }
+// var result = average(allNumbers2);
+// console.log(result);
 
 
 // Iteración #5: Calcular promedio de strings
 // Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+
+// RESUELTA!!!
 const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 function averageWord(param) {
-  // insert code
+    let total = 0;
+  for(var i = 0; i < param.length; i++){
+      if(typeof param[i] === "number"){
+          total += param[i];
+        } else {
+            total += param[i].length;
+        }
+    }
+    return total;
 }
+
+var result = averageWord(mixedElements);
+console.log("Este es el resultado de la Iteración #5: " + result);
 
 
 
